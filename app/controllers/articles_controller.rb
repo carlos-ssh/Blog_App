0863b995f1bd83e44ea579d53671e6105141942f
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-06 13:35:36 -0500

    Create a new link to welcome page.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
     def new
+        @article = Article.new
     end
 
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
     
     def show
