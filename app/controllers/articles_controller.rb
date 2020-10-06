ee005595b580c249d2d0b1f601e3576d10d6e115
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-06 13:16:04 -0500

    Create routes for articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article].inspect
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+
 end
