b75ef25aad35b83ab0de322ae4d22fdb1da4d2bd
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-06 16:03:29 -0500

    Create a new page with CRUD function Update with edit tag.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,16 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+                redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
     private
         def article_params
             params.require(:article).permit(:title, :text)
