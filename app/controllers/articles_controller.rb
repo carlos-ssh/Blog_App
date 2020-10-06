893cc897826037bdb9e8e8a0291e713e773252bf
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-06 13:21:58 -0500

    Create an index view for Articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,11 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
-    
+
+    def index
+        @articles = Article.all
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :text)
