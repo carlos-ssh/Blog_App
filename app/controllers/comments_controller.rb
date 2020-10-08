3763473c958c8bc97fb2f4e1fea9611d71e10f6d
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-08 12:49:38 -0500

    Working with login session.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+    #Create an authentication by user to delete comments
+    #http_basic_authenticate_with name: "Charly", password: "secret", only: :destroy
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
