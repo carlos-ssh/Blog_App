9b1d446ea23ecef0604706a1ddb56c1bcac640cd
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-09 23:40:20 -0500

    Users db migrated.

--- db/migrate/20201009184204_add_picture_to_images.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToImages < ActiveRecord::Migration[6.0]
-  def change
-    add_column :images, :picture, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_214649) do
+ActiveRecord::Schema.define(version: 2020_10_10_043854) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2020_10_06_214649) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "comments", "articles"
