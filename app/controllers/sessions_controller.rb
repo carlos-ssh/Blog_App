e590ec6d363dd37426f6f07bcbef1a23d4b591f6
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-12 16:16:42 -0500

    Authentication and validations.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged In!'
+    else
+      flash.now[:alert] = 'Email or Password Invalid'
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged Out!'
   end
 end
