d2e4d4497bf21f0f329402dc1564477f02eedfcf
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-12 09:38:50 -0500

    Add validation to the form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(params.require(:user).permit(:name, :email, :password))
+        @user = User.new user_params
+
+        if @user.save
+            return redirect_to new_user_path
+        end
+
+        render :new
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    
+    validates :email, presence: true, uniqueness: true
+    validates :name, :password, presence: true
 end
