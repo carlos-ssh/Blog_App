6958f66401a2ddc29f7c9f280aa973f6dcf8970d
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-12 14:43:19 -0500

    Create a new route for sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'post/index'
-  get 'welcome/index'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   root :to => "welcome#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
