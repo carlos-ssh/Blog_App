296204d4f87a444f7d3f6e444d5afbe98e3f89a4
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-12 09:09:41 -0500

    create a form and install bulma.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     def new
-        
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(params.require(:user).permit(:name, :email, :password))
     end
 end
