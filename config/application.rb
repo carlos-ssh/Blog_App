41b824eedf52ab4c3aee7bec1dededf048b44581
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-12 13:58:57 -0500

    Create a validation for new user page.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    I18n.available_locales = [:en, :de, :es]
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
