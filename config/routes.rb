d7a529c21b616066c1d0b691b768d75734445d39
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-13 18:37:48 -0500

    Update old components.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
- 
+
+  get 'figures/index'
+  get 'figures/new'
+  get 'figures/create'
+  get 'figures/edit'
+  get 'figures/update'
   root "welcome#index"
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
-
-  
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
   #Users
-  resources :users, only:[:new, :create]
+  resource :users, only:[:new, :create]
 
   #Articles
-  resources :articles do
-    resources :comments
-  end
+  resource :articles
 end
