c0e6e7005129dc9263ac61f05a9d1df1649566e1
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-13 16:10:02 -0500

    Users DB migrated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+ 
+  root "welcome#index"
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 
-  root :to => "welcome#index"
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
