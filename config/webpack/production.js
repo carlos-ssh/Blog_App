9f31ed97e9cffdb24f9daf0ee4ebe41dba1ea31f
Carlos Robles <carlos.anezeh@me.com>
2020-12-06 12:42:16 -0600

    Fixed issues in stylesheets folder.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+warnings: true
+
+NODE_ENV=production RAILS_ENV=production rails assets:precompile --trace
